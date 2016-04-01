b21ef7595c48d75e8973b5593154a641fcdc1a57
Anikesh Haran <anikeshharan@yahoo.in>
2016-04-01 18:50:47 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "starting deployment ....."
 #docker-compose up -d
 docker run --name mongodb -d mongo
 
-docker run -it --name nodejscontainer -p 3000:3000 -e "MONGODB_HOST=172.17.0.2" -e "MONGODB_PORT=27017" --link mongodb:mongo -d anikeshharan/nodejsapp
+docker run -it --name nodejscontainer -p 3000:3000 --link mongodb:mongo -d anikeshharan/nodejsapp
 
 echo "deployment completed...."
